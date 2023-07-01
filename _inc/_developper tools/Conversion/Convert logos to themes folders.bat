8c0bc6ca3543ce97b7af9acb80ae7a570cb993a8
lehcimcramtrebor <59354081+lehcimcramtrebor@users.noreply.github.com>
2023-07-02 00:22:48 +0200

    Update Convert logos to themes folders.bat

--- _inc/_developper tools/Conversion/Convert logos to themes folders.bat
+++ _inc/_developper tools/Conversion/Convert logos to themes folders.bat
@@ -2,33 +2,81 @@
 setlocal enabledelayedexpansion
 
 set "folderPath=%~dp0"
+set "suffixes=-ca -de -es -fr -it -nl -pt -ptbr -usa -us -en -us -eu -e -ue -euro -w -wor -world -wr -uk -spain -s -france -fre -french -f -germany -d -italy -i -netherlands -gr -greece -no -sw -sweden -se -portugal -pl -poland -jp -japan -ja -j -br -brazil -ru -r -kr -lorea -cn -china -hong -kong -ch -hk -as -tw -canada -c -fc -in -india"
 
 for %%G in ("%folderPath%*.png") do (
-    md "%%~nG" 2>nul
-    move /Y "%%G" "%%~nG\logo.png"
-    
-    (
-    echo ^<?xml version="1.0" encoding="UTF-8"?^>
-    echo ^<theme^>
-    echo ^<formatVersion^>7^</formatVersion^>
-    echo.
-    echo     ^<include^>./../theme.xml^</include^>
-    echo.
-    echo ^</theme^>
-    ) > "%%~nG\theme.xml"
+    set "filename=%%~nG"
+    set "suffix="
+    call :checkSuffix "!filename!" filename suffix
+    md "!filename!" 2>nul
+    if defined suffix (
+        move /Y "%%G" "!filename!\logo!suffix!.png" >nul
+    ) else (
+        move /Y "%%G" "!filename!\logo.png" >nul
+        (
+        echo ^<?xml version="1.0" encoding="UTF-8"?^>
+        echo ^<theme^>
+        echo ^<formatVersion^>7^</formatVersion^>
+        echo.
+        echo     ^<include^>./../theme.xml^</include^>
+        echo.
+        echo ^</theme^>
+        ) > "!filename!\theme.xml"
+    )
 )
 
 cd "%folderPath%\jp"
 for %%G in ("*.png") do (
-    md "%folderPath%%%~nG\jp" 2>nul
-    move /Y "%%G" "%folderPath%%%~nG\jp\logo.png"
+    set "filename=%%~nG"
+    set "suffix="
+    call :checkSuffix "!filename!" filename suffix
+    md "%folderPath%!filename!\jp" 2>nul
+    if defined suffix (
+        move /Y "%%G" "%folderPath%!filename!\jp\logo!suffix!.png" >nul
+    ) else (
+        move /Y "%%G" "%folderPath%!filename!\jp\logo.png" >nul
+    )
 )
 
 cd "%folderPath%\us"
 for %%G in ("*.png") do (
-    md "%folderPath%%%~nG\us" 2>nul
-    move /Y "%%G" "%folderPath%%%~nG\us\logo.png"
+    set "filename=%%~nG"
+    set "suffix="
+    call :checkSuffix "!filename!" filename suffix
+    md "%folderPath%!filename!\us" 2>nul
+    if defined suffix (
+        move /Y "%%G" "%folderPath%!filename!\us\logo!suffix!.png" >nul
+    ) else (
+        move /Y "%%G" "%folderPath%!filename!\us\logo.png" >nul
+    )
 )
 
-rd "%folderPath%\jp" 2>nul
-rd "%folderPath%\us" 2>nul
+cd "%folderPath%"
+rd "jp" /S /Q
+rd "us" /S /Q
+
+goto :eof
+
+:checkSuffix
+set "localFilename=%~1"
+set "localSuffix="
+for %%H in (%suffixes%) do (
+    if "!localFilename:~-2!"=="%%H" (
+        set "localSuffix=%%H"
+        set "localFilename=!localFilename:~0,-2!"
+        goto :setResult
+    ) else if "!localFilename:~-3!"=="%%H" (
+        set "localSuffix=%%H"
+        set "localFilename=!localFilename:~0,-3!"
+        goto :setResult
+    ) else if "!localFilename:~-4!"=="%%H" (
+        set "localSuffix=%%H"
+        set "localFilename=!localFilename:~0,-4!"
+        goto :setResult
+    )
+)
+
+:setResult
+set "%~2=!localFilename!"
+set "%~3=!localSuffix!"
+goto :eof
