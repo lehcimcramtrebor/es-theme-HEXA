6482cb13a79b503105554ca68da8eb090ac20532
lehcimcramtrebor <59354081+lehcimcramtrebor@users.noreply.github.com>
2023-07-29 08:19:47 +0200

    quake3

--- _inc/_developper tools/Windows/Developer Github scripts/Check others themes missing folders from HEXA.ps1
+++ _inc/_developper tools/Windows/Developer Github scripts/Check others themes missing folders from HEXA.ps1
@@ -4,7 +4,6 @@ $destinations = @(
     "$env:USERPROFILE\Documents\Github\es-theme-HEXA-HANDHELD",
     "$env:USERPROFILE\Documents\Github\es-theme-RCBX",
     "$env:USERPROFILE\Documents\Github\es-theme-batocer-club-reloaded",
-    "$env:USERPROFILE\Documents\Github\es-theme-butterfly",
     "$env:USERPROFILE\Documents\Github\es-theme-comicscrap",
     "$env:USERPROFILE\Documents\Github\es-theme-ComicScrapV2"
 )
