5ad6faac2ee37f39af685543d9db995c001f72dc
lehcimcramtrebor <59354081+lehcimcramtrebor@users.noreply.github.com>
2023-09-30 23:38:03 +0200

    dev

--- _inc/_developper tools/Linux/Developer Github scripts/Export logos to other themes.sh
+++ _inc/_developper tools/Linux/Developer Github scripts/Export logos to other themes.sh
@@ -11,6 +11,7 @@ dest4="$(xdg-user-dir DOCUMENTS)/GitHub/es-theme-forever/CUSTOMIZE/logos/"
 dest5="$(xdg-user-dir DOCUMENTS)/GitHub/es-theme-HEXA-HANDHELD/_inc/logos/"
 dest6="$(xdg-user-dir DOCUMENTS)/GitHub/es-theme-HUDGEM/_assets/logos/"
 dest7="$(xdg-user-dir DOCUMENTS)/GitHub/es-theme-RCBX/_inc/logo/"
+dest8="$(xdg-user-dir DOCUMENTS)/GitHub/es-theme-RGB/_inc/logos/"
 
 # Demande de confirmation
 read -p "Êtes-vous sûr de vouloir vider les dossiers de logos classiques des autres dépôts GitHub et d'y copier les logos depuis le dépôt HEXA? (O/N) " confirm
@@ -19,7 +20,7 @@ if [ ! "$confirm" = "O" ] && [ ! "$confirm" = "o" ]; then
 fi
 
 # Vide et recrée les dossiers de destination
-for dest in "$dest1" "$dest2" "$dest3" "$dest4" "$dest5" "$dest6" "$dest7"; do
+for dest in "$dest1" "$dest2" "$dest3" "$dest4" "$dest5" "$dest6" "$dest7" "$dest8"; do
     if [ -d "$dest" ]; then
         rm -rf "$dest"
     fi
@@ -27,7 +28,7 @@ for dest in "$dest1" "$dest2" "$dest3" "$dest4" "$dest5" "$dest6" "$dest7"; do
 done
 
 # Copie des fichiers de src vers chaque dest
-for dest in "$dest1" "$dest2" "$dest3" "$dest4" "$dest5" "$dest6" "$dest7"; do
+for dest in "$dest1" "$dest2" "$dest3" "$dest4" "$dest5" "$dest6" "$dest7" "$dest8"; do
     cp -r $src "$dest"
 done
 
